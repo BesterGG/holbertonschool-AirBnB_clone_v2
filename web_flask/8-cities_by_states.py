a6dcfbada8dafeba8989788531e8e1bd41578078
Bester <besster.gg@gmail.com>
2022-12-04 12:14:50 -0300

    Task8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -16,7 +16,7 @@ def teardown(exit):
 
 @app.route('/cities_by_states')
 def cities_x_states():
-    return render_template('8-cities_by_states.html', states=storage.all(State), city=storage.all(City))
+    return render_template('8-cities_by_states.html', states=storage.all(State))
 
 
 if __name__ == '__main__':
